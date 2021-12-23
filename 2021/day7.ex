2ea6172de1df8810fdb2d86237f8b1df8fa62a05
Alexey Belousov <belousov.a.v@ya.ru>
2021-12-23 23:02:34 +1000

    2021 day 7 part 1

---
+++ 2021/day7.ex
@@ -0,0 +1,17 @@
+crabs = File.read!("day7.txt") |> String.split(",") |> Enum.map(&String.to_integer(&1))
+
+{min, max} = Enum.min_max(crabs)
+
+res =
+  (min..max)
+  |> Enum.map(fn i ->
+    {
+      i,
+      crabs
+      |> Enum.map(&(abs(i - &1)))
+      |> Enum.sum()
+    }
+  end)
+  |> Enum.min_by(fn {_, steps} -> steps end)
+
+IO.inspect({"part1", res})
